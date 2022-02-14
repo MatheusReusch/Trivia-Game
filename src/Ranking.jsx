6d73853a0cf44966bf88fa269a270e96446587b1
Matheus Reusch <matheusreusch1@gmail.com>
2022-02-14 19:08:42 -0300

    req17

--- src/Ranking.jsx
+++ src/Ranking.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 class Ranking extends React.Component {
   render() {
+    const { history } = this.props;
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <div>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          Inicio
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: propTypes.func.isRequired,
+};
 
 export default Ranking;
