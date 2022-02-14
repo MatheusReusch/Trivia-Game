52064c6e9ee57380b8afeb122146ef8900675c18
Matheus Reusch <matheusreusch1@gmail.com>
2022-02-14 16:57:56 -0300

    Req 5 e 8

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,35 +1,58 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import propTypes from 'prop-types';
-import Header from './Header';
-import { updateQuestions } from './actions';
+import React from "react";
+import { connect } from "react-redux";
+import propTypes from "prop-types";
+import Header from "./Header";
+import { updateQuestions } from "./actions";
 
 class Game extends React.Component {
   constructor() {
     super();
-
+    const tres = 3;
     this.state = {
       indice: 0,
-      numero: Math.round(Math.random() * (3 - 0) + 0),
-      styleCorrect: '',
-      styleIncorrect: '',
+      numero: Math.round(Math.random() * (tres - 0) + 0),
+      styleCorrect: "",
+      styleIncorrect: "",
+      timer: 30,
+      disabled: false,
     };
   }
 
-  corCorreta = () => {
-    this.setState({ styleCorrect: '3px solid rgb(6, 240, 15)' });
+  componentDidMount() {
+    setInterval(() => {
+      this.setState(
+        (prevState) => ({ timer: prevState.timer - 1 }),
+        () => {
+          const { timer } = this.state;
+          if (timer <= 0) {
+            this.setState({ disabled: true });
+          }
+        }
+      );
+    }, 1000);
   }
+
+  corCorreta = () => {
+    this.setState({ styleCorrect: "3px solid rgb(6, 240, 15)" });
+  };
 
   corIncorreta = () => {
-    this.setState({ styleIncorrect: '3px solid rgb(255, 0, 0)' });
-  }
+    this.setState({ styleIncorrect: "3px solid rgb(255, 0, 0)" });
+  };
+
+  cor = () => {
+    this.corCorreta();
+    this.corIncorreta();
+  };
 
   render() {
-    const { indice, numero, styleCorrect, styleIncorrect } = this.state;
+    const { indice, numero, styleCorrect, styleIncorrect, timer, disabled } =
+      this.state;
     const { questions } = this.props;
     return (
       <div>
         <Header />
+        <h2>{timer}</h2>
         {questions.length > 0 && numero <= 1 ? (
           <>
             <h2 data-testid="question-category">
@@ -37,41 +60,86 @@ class Game extends React.Component {
             </h2>
             <h2 data-testid="question-text">{questions[indice].question}</h2>
             <section data-testid="answer-options">
-              <button type="button" onClick={ () => this.corCorreta() } style={ { border: styleCorrect } } data-testid="correct-answer">
-                {  questions[indice].correct_answer}
+              <button
+                type="button"
+                disabled={disabled}
+                onClick={() => this.cor()}
+                style={{ border: styleCorrect }}
+                data-testid="correct-answer"
+              >
+                {questions[indice].correct_answer}
               </button>
-              <button type="button" style={ { border: styleIncorrect } } data-testid="wrong-answer-0">
+              <button
+                type="button"
+                onClick={() => this.cor()}
+                style={{ border: styleIncorrect }}
+                data-testid="wrong-answer-0"
+              >
                 {questions[indice].incorrect_answers[0]}
               </button>
-              <button type="button" style={ { border: styleIncorrect } } data-testid="wrong-answer-1">
+              <button
+                type="button"
+                onClick={() => this.cor()}
+                style={{ border: styleIncorrect }}
+                data-testid="wrong-answer-1"
+              >
                 {questions[indice].incorrect_answers[1]}
               </button>
-              <button type="button" style={ { border: styleIncorrect } } data-testid="wrong-answer-2">
+              <button
+                type="button"
+                onClick={() => this.cor()}
+                style={{ border: styleIncorrect }}
+                data-testid="wrong-answer-2"
+              >
                 {questions[indice].incorrect_answers[2]}
               </button>
             </section>
           </>
-        ) : (questions.length > 0 && <>
-        <h2 data-testid="question-category">
-          {questions[indice].category}
-        </h2>
-        <h2 data-testid="question-text">{questions[indice].question}</h2>
-        <section data-testid="answer-options">
-          <button type="button" style={ { border: styleIncorrect } } data-testid="wrong-answer-2">
-            {questions[indice].incorrect_answers[2]}
-          </button>
-          <button type="button" onClick={ () => this.corCorreta() } style={ { border: styleCorrect } } data-testid="correct-answer">
-            {  questions[indice].correct_answer}
-          </button>
-          <button type="button" style={ { border: styleIncorrect } } data-testid="wrong-answer-0">
-            {questions[indice].incorrect_answers[0]}
-          </button>
-          <button type="button" style={ { border: styleIncorrect } } data-testid="wrong-answer-1">
-            {questions[indice].incorrect_answers[1]}
-          </button>
-        </section>
-      </>)
-      }
+        ) : (
+          questions.length > 0 && (
+            <>
+              <h2 data-testid="question-category">
+                {questions[indice].category}
+              </h2>
+              <h2 data-testid="question-text">{questions[indice].question}</h2>
+              <section data-testid="answer-options">
+                <button
+                  type="button"
+                  onClick={() => this.cor()}
+                  style={{ border: styleIncorrect }}
+                  data-testid="wrong-answer-2"
+                >
+                  {questions[indice].incorrect_answers[2]}
+                </button>
+                <button
+                  type="button"
+                  onClick={() => this.cor()}
+                  style={{ border: styleCorrect }}
+                  disabled={disabled}
+                  data-testid="correct-answer"
+                >
+                  {questions[indice].correct_answer}
+                </button>
+                <button
+                  type="button"
+                  onClick={() => this.cor()}
+                  style={{ border: styleIncorrect }}
+                  data-testid="wrong-answer-0"
+                >
+                  {questions[indice].incorrect_answers[0]}
+                </button>
+                <button
+                  type="button"
+                  onClick={() => this.cor()}
+                  style={{ border: styleIncorrect }}
+                  data-testid="wrong-answer-1"
+                >
+                  {questions[indice].incorrect_answers[1]}
+                </button>
+              </section>
+            </>
+          )
+        )}
       </div>
     );
   }
@@ -82,8 +150,8 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const mapStateToProps = (state) => ({
-    questions: state.questoes,
-})
+  questions: state.questoes,
+});
 
 Game.propTypes = {
   questions: propTypes.func.isRequired,
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
-import md5 from "crypto-js/md5";
-import propTypes from "prop-types";
-import { getToken, updateQuestions, updateToken } from "./actions";
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
+import propTypes from 'prop-types';
+import { getToken, updateQuestions, updateToken } from './actions';
 
 function Login(props) {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   const [disabled, setDisabled] = useState(true);
-  const { updateToken } = props;
+  const { updateToken2 } = props;
 
   useEffect(() => {
-    if (email !== "" && name !== "") {
+    if (email !== '' && name !== '') {
       setDisabled(false);
     } else {
       setDisabled(true);
@@ -23,50 +23,50 @@ function Login(props) {
       <input
         data-testid="input-player-name"
         type="text"
-        onChange={(event) => setName(event.target.value)}
+        onChange={ (event) => setName(event.target.value) }
       />
       <input
         data-testid="input-gravatar-email"
         type="text"
-        onChange={(event) => setEmail(event.target.value)}
+        onChange={ (event) => setEmail(event.target.value) }
       />
       <button
         type="button"
         data-testid="btn-play"
-        disabled={disabled}
-        onClick={async () => {
+        disabled={ disabled }
+        onClick={ async () => {
           const hash = md5(email).toString();
           console.log(hash);
-          await fetch("https://opentdb.com/api_token.php?command=request")
+          await fetch('https://opentdb.com/api_token.php?command=request')
             .then((response) => response.json())
             .then((data) => {
-              localStorage.setItem("token", data.token);
-              updateToken(hash, name, email, data);
+              localStorage.setItem('token', data.token);
+              updateToken2(hash, name, email, data);
             });
-          console.log(localStorage.getItem("token"));
+          console.log(localStorage.getItem('token'));
           fetch(
             `https://opentdb.com/api.php?amount=5&token=${localStorage.getItem(
-              "token"
-            )}`
+              'token',
+            )}`,
           )
             .then((response) => response.json())
             .then((data) => {
               if (data.results.length !== 0) {
                 props.setQuestions(data.results);
               } else {
-                console.log("errou");
+                console.log('errou');
                 props.pegarToken(hash, name, email);
                 fetch(
                   `https://opentdb.com/api.php?amount=5&token=${localStorage.getItem(
-                    "token"
-                  )}`
+                    'token',
+                  )}`,
                 )
                   .then((response) => response.json())
-                  .then((data) => props.setQuestions(data.results));
+                  .then((data2) => props.setQuestions(data2.results));
               }
             });
-          props.history.push("/game");
-        }}
+          props.history.push('/game');
+        } }
       >
         Jogar
       </button>
@@ -85,14 +85,15 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   pegarToken: (hash, name, email) => dispatch(getToken(hash, name, email)),
   setQuestions: (questoes) => dispatch(updateQuestions(questoes)),
-  updateToken: (hash, name, email, data) => dispatch(updateToken(hash, name, email, data))
+  updateToken2: (hash, name, email,
+    data) => dispatch(updateToken(hash, name, email, data)),
 });
 
 Login.propTypes = {
   setQuestions: propTypes.func.isRequired,
   pegarToken: propTypes.func.isRequired,
   history: propTypes.func.isRequired,
-  token: propTypes.string.isRequired,
+  updateToken2: propTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
